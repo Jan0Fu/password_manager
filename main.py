72717fee90bba98d8e51ec41143029f60eeca838
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-08 17:07:31 +0100

    Completed main app functionality

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
 from tkinter import *
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
+
+def save():
+    website = web_entry.get()
+    email = user_entry.get()
+    password = pass_entry.get()
+    with open("data.txt", "a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+        web_entry.delete(0, END)
+        pass_entry.delete(0, END)
+
+
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=lock_img)
+canvas.create_image(127, 100, image=lock_img)
 canvas.grid(column=1, row=0)
 
 web_label = Label(text="Website:")
 web_label.grid(column=0, row=1)
-
-user_label = Label(text="Email/Username")
+user_label = Label(text="Email/Username:")
 user_label.grid(column=0, row=2)
-
-pass_label = Label(text="Password")
+pass_label = Label(text="Password:")
 pass_label.grid(column=0, row=3)
 
-web_entry = Entry(width=35)
+web_entry = Entry(width=38)
 web_entry.grid(column=1, columnspan=2, row=1)
-
-user_entry = Entry(width=35)
+web_entry.focus()
+user_entry = Entry(width=38)
 user_entry.grid(column=1, columnspan=2, row=2)
+user_entry.insert(END, "janivstar@gmail.com")
 
-pass_entry = Entry(width=18)
+pass_entry = Entry(width=21)
 pass_entry.grid(column=1, row=3)
-
 generate_button = Button(text="Generate Password")
 generate_button.grid(column=2, row=3)
 
-generate_button = Button(text="Add", width=36)
-generate_button.grid(column=1, columnspan=2, row=4)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, columnspan=2, row=4)
 
 window.mainloop()
