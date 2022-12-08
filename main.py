14631e1fcd696dafdbb6a6c742c95b43d6aaa58d
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-08 20:48:42 +0100

    Added new features and improvements

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(letters) for _ in range(randint(2, 4))]
+    password_numbers = [choice(letters) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+    password = "".join(password_list)
+    pass_entry.insert(0, password)
+    pyperclip.copy(password)
 
 
 def save():
     website = web_entry.get()
     email = user_entry.get()
     password = pass_entry.get()
-    with open("data.txt", "a") as file:
-        file.write(f"{website} | {email} | {password}\n")
-        web_entry.delete(0, END)
-        pass_entry.delete(0, END)
+    if len(website) == 0 or len(password) < 3:
+        messagebox.showinfo(title="Oops", message="Please enter valid input!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                  f"\npassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+                web_entry.delete(0, END)
+                pass_entry.delete(0, END)
 
 
 window = Tk()
@@ -37,9 +61,9 @@ user_entry.insert(END, "janivstar@gmail.com")
 
 pass_entry = Entry(width=21)
 pass_entry.grid(column=1, row=3)
-generate_button = Button(text="Generate Password")
-generate_button.grid(column=2, row=3)
 
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(column=2, row=3)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, columnspan=2, row=4)
 
