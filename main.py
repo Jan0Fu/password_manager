c19b0daa1c4bc379925388286df1478f35f4fc31
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-08 20:51:53 +0100

    Fixed bug

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def generate_password():
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
-    password_symbols = [choice(letters) for _ in range(randint(2, 4))]
-    password_numbers = [choice(letters) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
     password_list = password_letters + password_symbols + password_numbers
     shuffle(password_list)
